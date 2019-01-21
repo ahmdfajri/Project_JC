984fca71e5c02f8217d89b3d601cbdd7a6f77b4a
ahmdfajri <achmad.fajria14@gmail.com>
2019-01-21 12:48:33 +0700

    update versi 2

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,6 +7,8 @@ import Collections from '../Pages/Collections';
 import New from '../Pages/New';
 import Sale from '../Pages/Sale';
 import Store from '../Pages/Store';
+import Signin from '../Pages/Signin';
+import Signup from '../Pages/Signup';
 
 const Main = () => (
     <Switch>
@@ -17,6 +19,8 @@ const Main = () => (
         <Route path="/New" component={New}/>
         <Route path="/Sale" component={Sale}/>
         <Route path="/Store" component={Store}/>
+        <Route path="/Signin" component={Signin}/>
+        <Route path="/Signup" component={Signup}/>
     </Switch>
 )
 
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,31 +7,43 @@ class Nav extends Component {
     render() {
         return (
             <div>
-                <Link to="/"><img className="header_img" src="./img/image02.jpg" alt=""/></Link>
-            <div className="demo-big-content">
-                <Layout>
-                    <Header className="header_color">
-                        <Navigation>
-                            <Link to="/New">NEW ARRIVAL</Link>
-                            <Link to="/Categories">CATEGORIES</Link>
-                            <Link to="/Collections">COLLECTIONS</Link>
-                            <Link to="/Sale">SALE</Link>
-                            <Link to="/Gift">GIFT</Link>
-                            <Link to="/Store">OUR STORE</Link>
-                            <Textfield
-                                onChange={() => { }}
-                                label="Expandable Input"
-                                expandable
-                                expandableIcon="search"
-                            />
-                        </Navigation>
-                    </Header>
-                    <Content>
-                        <div className="page-content" />
-                        <Main />
-                    </Content>
-                </Layout>
-            </div>
+                <Link to="/"><img className="header_img" src="./img/image02.jpg" alt="" /></Link>
+                <div className="demo-big-content">
+                    <Layout>
+                        <Header className="header_color">
+                            <Navigation>
+                                <Link to="/New">NEW ARRIVAL</Link>
+                                <Link to="/Categories">CATEGORIES
+                                    <button id="demo-menu-lower-left"
+                                        class="mdl-button mdl-js-button mdl-button--icon">
+                                        <i class="material-icons">more_vert</i>
+                                    </button>
+                                    <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
+                                        for="demo-menu-lower-left">
+                                        <li class="mdl-menu__item">Woman</li>
+                                        <li class="mdl-menu__item">Kid</li>
+                                        <li class="mdl-menu__item">Baby</li>
+                                    </ul>
+                                </Link>
+                                <Link to="/Collections">COLLECTIONS</Link>
+                                <Link to="/Sale">SALE</Link>
+                                <Link to="/Gift">GIFT</Link>
+                                <Link to="/Store">OUR STORE</Link>
+                                <Textfield
+                                    onChange={() => { }}
+                                    label="Expandable Input"
+                                    expandable
+                                    expandableIcon="search"
+                                />
+                                <Link to="/Signin">Sign In</Link>
+                            </Navigation>
+                        </Header>
+                        <Content>
+                            <div className="page-content" />
+                            <Main />
+                        </Content>
+                    </Layout>
+                </div>
             </div>
 
         );
